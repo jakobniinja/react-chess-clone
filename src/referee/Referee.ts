4e82046a8cf0755d4be7bb81ef34b3a55c28e178
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-06-04 13:42:08 +0200

    fix: impl basic pawn role

--- src/referee/Referee.ts
+++ src/referee/Referee.ts
@@ -35,30 +35,20 @@ export default class Referee {
     if (type === PieceType.PAWN) {
       const specialRow = team === TeamType.OUR ? 1 : 6;
       const pawnDirection = team === TeamType.OUR ? 1 : -1;
-      if (py === specialRow) {
-        if (px === x && y - py ===  pawnDirection ) {
-          if (!this.tileIsOccupied(x, y, boardState)) {
-            return true;
-          }
-          } else if (px === x && y - py === 2 * pawnDirection) {
-            if (
-              !this.tileIsOccupied(x, y, boardState) &&
-              !this.tileIsOccupied(x, y - pawnDirection, boardState)
-            ) {
-              return true;
-            }
-          }
-        } else {
-          if (px === x && y - py === pawnDirection) {
-            if (!this.tileIsOccupied(x, y, boardState)) {
-              return true;
-            }
-          }
+
+      if (px === x && py === specialRow && y - py === 2 * pawnDirection) {
+        if (
+          !this.tileIsOccupied(x, y, boardState) &&
+          !this.tileIsOccupied(x, y - pawnDirection, boardState)
+        ) {
+          return true;
+        }
+      } else if (px === x && y - py === pawnDirection) {
+        if (!this.tileIsOccupied(x, y, boardState)) {
+          return true;
         }
       }
+    }
     return false;
-    
-    
-
   }
 }
